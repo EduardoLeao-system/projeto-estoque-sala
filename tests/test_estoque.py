90de8bfc4d21d066f9f4e691e7e5c1c86f2e925f
Usuário <carloseduardomeirelesleao@gmail.com>
2025-10-17 20:38:36 -0400

    coverage, arquivos de estoque

--- tests/test_estoque.py
+++ tests/test_estoque.py
@@ -6,3 +6,15 @@ def test_adicionar_produto_simples():
     e = Estoque()
     e.adicionar_produto("Arroz", 10.0, 5)
     assert e.consultar_produto("Arroz") == {"preco": 10.0, "quantidade":5}
+
+@pytest.mark.estoque
+def test_reAdicionar_soma_quantidade_mantem_preco():
+    e = Estoque()
+    e.adicionar_produto("Arroz", 10.0, 5)
+    e.adicionar_produto("Arroz", 90.0, 3)
+    assert e.consultar_produto("Arroz") == {"preco": 10.0, "quantidade": 8}
+
+@pytest.mark.estoque
+def test_remover_produto_sucesso():
+    e = Estoque()
+    e.adicionar_produto("Feijao":)
