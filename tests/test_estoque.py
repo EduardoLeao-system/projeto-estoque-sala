2114823fe6e37869288e01747c4ae8c13da5e2dc
Usuário <carloseduardomeirelesleao@gmail.com>
2025-10-17 20:22:22 -0400

    teste e arquivos de estoque

--- tests/test_estoque.py
+++ tests/test_estoque.py
@@ -2,4 +2,7 @@ import pytest
 from src.estoque import Estoque
 
 @pytest.mark.estoque
-def test_adicionar
+def test_adicionar_produto_simples():
+    e = Estoque()
+    e.adicionar_produto("Arroz", 10.0, 5)
+    assert e.consultar_produto("Arroz") == {"preco": 10.0, "quantidade":5}
